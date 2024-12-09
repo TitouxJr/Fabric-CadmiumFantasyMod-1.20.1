5e53fcc215cd4acff252e02cc81e11d599c5c602
TitouxJr <111535159+TitouxJr@users.noreply.github.com>
2024-12-09 20:57:24 +0100

    - ModBlockTagProvider - ModItemTagProvider - ModLootTableProvider

--- src/main/java/net/titoux/cadmiumfantasymod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/titoux/cadmiumfantasymod/datagen/ModBlockTagProvider.java
@@ -2,8 +2,11 @@ package net.titoux.cadmiumfantasymod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import net.titoux.cadmiumfantasymod.block.ModBlocks;
 import net.titoux.cadmiumfantasymod.util.ModTags;
 
@@ -29,8 +32,24 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
         getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
                 .add(ModBlocks.CADMIUM_ORE)
+                .add(ModBlocks.CADMIUM_BLOCK)
                 .add(ModBlocks.DEEPSLATE_CADMIUM_ORE)
                 .add(ModBlocks.NETHER_CADMIUM_ORE)
+                .add(ModBlocks.END_CADMIUM_ORE)
+                .add(ModBlocks.SOUND_BLOCK);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_STONE_TOOL)
+                .add(ModBlocks.CADMIUM_BLOCK);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.CADMIUM_ORE)
+                .add(ModBlocks.NETHER_CADMIUM_ORE);
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.SOUND_BLOCK)
+                .add(ModBlocks.DEEPSLATE_CADMIUM_ORE);
+
+        getOrCreateTagBuilder(TagKey.of(RegistryKeys.BLOCK, new Identifier("fabric", "needs_tool_level_4")))
                 .add(ModBlocks.END_CADMIUM_ORE);
     }
 }
--- src/main/java/net/titoux/cadmiumfantasymod/datagen/ModLootTableProvider.java
+++ src/main/java/net/titoux/cadmiumfantasymod/datagen/ModLootTableProvider.java
@@ -1,4 +1,44 @@
 package net.titoux.cadmiumfantasymod.datagen;
 
-public class ModLootTableProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.block.Block;
+import net.minecraft.data.server.loottable.BlockLootTableGenerator;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LeafEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.titoux.cadmiumfantasymod.block.ModBlocks;
+import net.titoux.cadmiumfantasymod.item.ModItems;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+    protected ModLootTableProvider(FabricDataOutput dataOutput) {
+        super(dataOutput);
+    }
+
+    @Override
+    public void generate() {
+        addDrop(ModBlocks.CADMIUM_BLOCK);
+        addDrop(ModBlocks.SOUND_BLOCK);
+
+        addDrop(ModBlocks.CADMIUM_ORE, copperLikeOreDrops(ModBlocks.CADMIUM_ORE, ModItems.RAW_CADMIUM));
+        addDrop(ModBlocks.DEEPSLATE_CADMIUM_ORE, copperLikeOreDrops(ModBlocks.DEEPSLATE_CADMIUM_ORE, ModItems.RAW_CADMIUM));
+        addDrop(ModBlocks.NETHER_CADMIUM_ORE, copperLikeOreDrops(ModBlocks.NETHER_CADMIUM_ORE, ModItems.RAW_CADMIUM));
+        addDrop(ModBlocks.END_CADMIUM_ORE, copperLikeOreDrops(ModBlocks.END_CADMIUM_ORE, ModItems.RAW_CADMIUM));
+    }
+
+    public LootTable.Builder copperLikeOreDrops(Block drop, Item item) {
+        return BlockLootTableGenerator.dropsWithSilkTouch(drop, (LootPoolEntry.Builder) this.applyExplosionDecay(drop,
+                ((LeafEntry.Builder)
+                        ItemEntry.builder(item)
+                                .apply(SetCountLootFunction
+                                        .builder(UniformLootNumberProvider
+                                                .create(2.0F, 5.0F))))
+                        .apply(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE))));
+    }
 }
